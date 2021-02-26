82af27b5630fae9e957b9c27d5d3b90d244984a9
Diogo de Andrade <diogo.g.andrade@gmail.com>
2021-02-26 19:16:33 +0000

    Added assembly file for easy integration with Unity

---
+++ Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathlib
+{
+    public static class Extensions
+    {
+        public static float Range(this System.Random rnd, float valMin, float valMax)
+        {
+            return (float)(rnd.NextDouble() * (valMax - valMin) + valMin);
+        }
+    }
+}
